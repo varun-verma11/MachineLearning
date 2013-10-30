ca60fce2365c5c8f8e82e561989d398e79ec135d
Yangfan Zhang <yangfan.zhang11@imperial.ac.uk>
2013-10-30 23:39:17 +0000

    added cross validation. To be tested until information_gain is finished.

--- task1/load_data.m
+++ task1/load_data.m
@@ -1,14 +1,19 @@
-function [ x, y ] = load_data( desired_label )
-% function for Loading Data
-% This function takes in a label of the emotion for which the decision 
-% tree is to be trained, and returns the remapped clean data [x,y].
+function [ y ] = load_data( y, desired_label )
 
-%var x = load('cleandata_students.mat')
-load ('cleandata_students.mat')
-N = size(y)
+% function for Loading Data
+
+% INPUT
+% y is a N*1 vector, containing the labels of the corresponding examples
+% desired_y is the desired label of the emotion for which 
+% the decision tree is to be trained
+
+% OUTPUT
+% a N*1 vector, which is the remapped binary targets [y].
+
+N = size(y, 1);
 
 for i=1:N
-    y(i) = y(i) == desired_label
+    y(i) = y(i) == desired_label;
 end
 
 end
