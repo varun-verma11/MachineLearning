605140cbc4a3607b24ecd2154c7994ed926584bc
Yangfan Zhang <yangfan.zhang11@imperial.ac.uk>
2013-11-20 20:24:48 +0000

    added varun code

--- task2/cross_validation.m
+++ task2/cross_validation.m
@@ -4,7 +4,7 @@
 % Here, just input x,y don't need to use ANNdata
 % Output the best network
 function [result] = cross_validation(instances, labels, index, K, type)
-    training_functions = {'trainbfg','traincgb','traincgf','traincgp','traingda','traingdm','traingdx','trainlm','trainoss','trainrp','trainscg'};
+    training_functions = {'trainbfg'} %,'traincgb','traincgf','traincgp','traingda','traingdm','traingdx','trainlm','trainoss','trainrp','trainscg'};
     % we will find the best number of layers, the best number of netrons in
     % each layer, training functions and best topology
     best_n = 3;
@@ -34,11 +34,11 @@ function [result] = cross_validation(instances, labels, index, K, type)
                 %Train topology, for 1-3 see createNetwork
                 for t=3:3
                     %optimise first hidden layers max 3 neturons in this layer
-                    for a=1:3
+                    for a=3:3   %1
                             % optimise second layers as well 
-                            for b=0:3
+                            for b=3:3 % 0
                                 %optimise third layers
-                               for c=0:3
+                               for c=3:3  %0
                                     layers = [a,b,c];
                                     temp_n = layers(layers~=0);
                                     best_training_function_local = '';
@@ -48,17 +48,21 @@ function [result] = cross_validation(instances, labels, index, K, type)
                                         %create a network
                                         temp_net = createNetwork(temp_n,tX,tY,t);
                                           % configure parameters for the nueral network
+                                            DDF = 'divideind';
                                             temp_net.divideFcn = DDF;
+                                     
                                             temp_net.trainParam.epochs = 100;
                                             temp_net.trainParam.show = 5;
                                             temp_net.trainParam.lr = 0.001;
                                             size_t_y = length(trainY);
                                             temp_net.divideParam.trainInd = 1:size_t_y;
                                             temp_net.divideParam.valInd   = size_t_y+1:length(validY)+size_t_y;
-                                            [ann_trained, error_rate] = train(temp_net, [tX;vX], [tY;vY]);
-                                            fprintf('Error Rate: %d', error_rate);
+                                            [ann_trained, ann_para] = train(temp_net, horzcat(tX,vX), horzcat(tY,vY));
+                                            %fprintf('Error Rate: %d', error_rate);
+                                            display(ann_para.best_perf);
                                         %caculate error rate 
-%                                         temp_err = findErrorRate(getPredictions(temp_net, vX, type),validY);
+                                            temp_err = ann_para.best_perf;
+                                            %temp_err = findErrorRate(getPredictions(temp_net, vX, type),validY);
                                         if(temp_err < best_net_err_local)
                                             best_training_function_local = training_functions{z};
                                             best_net_err_local = temp_err;
@@ -92,21 +96,25 @@ function [result] = cross_validation(instances, labels, index, K, type)
             [testX_,testY_] = ANNdata(testX,testY);
             [train_X,train_Y] = ANNdata(trainX,trainY);
             if(type~=0)
-                    testY = transpose(load_data(testY,type));
+                    testY_ = transpose(load_data(testY,type));
                     train_Y = transpose(load_data(trainY,type));
             end
+            
             net = createNetwork(best_n,train_X,train_Y,best_topology);
             net.trainFcn = best_training_function;
-            net.trainParam.epochs = 300;
+            net.trainParam.epochs = 100;
             net.trainParam.showWindow = false;
             net = train(net,train_X, train_Y);
-                
-            display(1-findErrorRate(getPredictions(net, testX,type),testY));
+            
+            %display(length(testX_));
+            %display(length(testY_));
+            %display(testY_(1));
+            %display(1 - mse(net, train_X, train_Y));
+            display(1-findErrorRate(getPredictions(net, testX_,type),testY));
     
     end
     %return a network that trains on the entire data set 
     result = createNetwork(best_n, instances,labels, best_topology);
-    
 end
 
 function [trainX, trainY, validX, validY] = ...
