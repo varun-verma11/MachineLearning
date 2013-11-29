f62808dc88b8c1304f80daa7570ed382a9acc957
Varun Verma <vv311@ic.ac.uk>
2013-11-29 13:09:07 +0000

    adding dummy function or similarity

---
+++ task3/retrieve.m
@@ -0,0 +1,7 @@
+function [ most_similar_case ] = retrieve( cbr, case_find )
+%RETRIEVE Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ task3/similarity_of_cases.m
@@ -0,0 +1,7 @@
+function [ similarity ] = similarity_of_cases( case_one, case_two )
+%SIMILARITY_OF_CASES Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
