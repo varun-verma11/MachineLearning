5c42bdfdf501a34aff7eb47322cdba0adbb61c4e
Varun Verma <vv311@ic.ac.uk>
2013-11-29 12:39:22 +0000

    small change to a comment

--- task3/new_case.m
+++ task3/new_case.m
@@ -1,6 +1,6 @@
 function [ new_case ] = new_case( example, target )
 
-% safety check: elements in example are in range [0,1]
+% check all elements are either 0 or 1
 assert(isempty(example(example<0)));
 assert(isempty(example(example>1)));
 
